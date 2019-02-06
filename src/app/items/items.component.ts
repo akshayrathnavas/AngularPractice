a8ea93811af225ce6c69bca3eb5f2321b75f6609
AkshayRathnavas <akshayrathnavas@gmail.com>
2019-02-06 13:54:54 +0530

    To-Do List Two Way Binding App Part 2

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from 'util';
 
 @Component({
   selector: 'app-items',
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
   public task1 : String = "Sample";
-  public color1 : String = "blue"
+  public color1 : String = "yellow"
   public items: any=[];
+  public completedItems: any=[];
   constructor() { 
   }
 
@@ -23,6 +25,29 @@ export class ItemsComponent implements OnInit {
         "color":this.color1
     };
     this.items.push(obj1);
+    console.log(this.items);
+  }
+  enterEvent(event){
+    if(event.keyCode=='13'){
+      this.addTask();
+    }
+  }
+  doneTask(index){
+    if(this.items.length==1 &&  index=='0'){
+      this.completedItems.push(this.items[index]);
+      this.items=[];
+      return;
+    }
+    else if(index=='0'){
+      this.completedItems.push(this.items[index]);
+      this.items.splice(0,1);
+      return;
+    }
+    console.log(index);
+    console.log("length "+ this.items.length);
+    console.log(this.items[index]);
+    this.completedItems.push(this.items[index])
+    this.items.splice(index,index);
   }
 
 }
